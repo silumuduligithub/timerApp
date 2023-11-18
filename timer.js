c30aabb611201f6f36bd221a2200ed5c0c059eef
silulitgithub <kumarsilu530@gmail.com>
2023-11-18 18:29:24 +0530

    code push

--- timer.js
+++ timer.js
@@ -1,88 +1,67 @@
 // store all the timers
-let arr = [];
-arr.push("timer");
-let myInterval;
-let i = 65;
-let l;
-// console.log(arr[0]);
-function startTimer() {
-    myInterval = setInterval(myTimer, 1000);
-}
-function myTimer() {
-  let element = document.getElementById(arr[0]).innerText;
-  let time = element.split(":");
-  let hour = parseInt(time[0]);
-  let min = parseInt(time[1]);
-  let second = parseInt(time[2]);
-  let i = 0;
-  let totalSecond = hour * 60 * 3600 + min * 60 + second;
-  if (totalSecond == 1) {
-    myStopFunction();
+let i = 0, j = 0;
+let audio, interval;
+function startTimer(event) {
+  playAudioAlert(event);
+  j++;
+  interval = setInterval(() => {
+    let btn = event.id;
+    event.innerText = "Delete";
+    let child = document.getElementById(btn);
+    let parent = child.parentNode;
+    let timer = document.getElementById("-" + btn);
+    let time = timer.innerText;
+    const timeArr = time.split(":");
+    let hour = parseInt(timeArr[0]) * 3600;
+    let minute = parseInt(timeArr[1]) * 60;
+    let second = parseInt(timeArr[2]);
+    let totalTimeInSecond = hour + minute + second;
+    totalTimeInSecond--;
+    if (totalTimeInSecond == 0) {
+      parent.removeChild(parent.childNodes[0]);
+      parent.removeChild(parent.childNodes[1]);
+      parent.removeChild(parent.childNodes[2]);
+      audio.pause();
+      parent.style.backgroundColor = "yellow";
+      parent.innerHTML = `<h3>Timer Is Up !!</h3><button type="button" class="btn btn-secondary" style = "color : white; background-color : black">Stop</button>`;
+      clearInterval(interval);
+    }
+    let curHour = parseInt(totalTimeInSecond / 3600);
+    let curMinute = parseInt((totalTimeInSecond % 3600) / 60);
+    let curSecond = totalTimeInSecond % 60;
+    timer.innerText = curHour + " : " + curMinute + " : " + curSecond;
+  }, 1000);
+  if (j == 2) {
+    let btn = event.id;
+    let child = document.getElementById(btn);
+    let parent = child.parentNode;
+    parent.remove(0);
+    audio = null;
+    clearInterval(interval);
     return;
   }
-  if (totalSecond > 1) {
-    if (second < 1) {
-      min -= 1;
-      second = 60;
-    }
-    if (min < 0) {
-      hour -= 1;
-      min = 59;
-    }
-    second--;
-    totalSecond--;
-
-    document.getElementById(arr[0]).innerHTML = hour + ":" + min + ":" + second;
-  }
-}
-
-function myStopFunction() {
-  clearInterval(myInterval);
 }
 
 // lets add the timer
 function addTimer() {
+  i++;
   let container = document.getElementById("parent");
   let card = document.createElement("div");
-  card.contentEditable = true;
-  card.style.display = "flex";
-  card.style.width = "30vw";
-  card.style.height = "5vh";
-  card.style.top = "132px";
-  card.style.borderRadius = "20px";
-  card.style.marginLeft = "5vw";
-  card.style.marginTop = "20px";
-  card.style.alignItems = "center";
-  card.style.justifyContent = "space-between";
-  card.style.paddingInline = "30px";
-  card.style.boxSizing = "border-box";
-  card.style.padding = "20px";
-  card.style.backgroundColor = "#34344a";
-  card.id = "card" + String.fromCharCode(i);
-  console.log(card.id);
-  arr.push(card.id);
-  i++;
+  card.id = "card";
+  let cardId = "-" + i;
+  let buttonId = i + "";
+
+  card.innerHTML = `<h4>Set Time</h4>
+  <div class="timer"  contenteditable="true" id = ${cardId} >00:00:00</div>
+  <button onclick="startTimer(this)" id = ${buttonId}>Set</button>`;
   container.appendChild(card);
-
-  let h4 = document.createElement("h4");
-  h4.innerText = "Set Time";
-  h4.style.color = "white";
-  card.appendChild(h4);
-
-  let timer = document.createElement("div");
-  timer.innerText = "00:00:00";
-  timer.style.color = "white";
-  timer.style.backgroundColor = "#34344a";
-  timer.id = "timer";
-  card.appendChild(timer);
-
-  let btn = document.createElement("button");
-  btn.innerText = "Set";
-  btn.addEventListener("click", startTimer);
-  btn.style.borderRadius = "10%";
-  btn.style.backgroundColor = "#f0f757";
-  btn.style.width = "80px";
-  btn.style.height = "30px";
-  btn.style.border = "none";
-  card.appendChild(btn);
 }
+function playAudioAlert(event) {
+  if (event.innerText != "Delete") {
+    audio = new Audio("./timerMusic.mp3"); // Replace with the path to your audio file
+    audio.play();
+  } else {
+    audio = null;
+  }
+}
+addTimer();
